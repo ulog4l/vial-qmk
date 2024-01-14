e6cae7bd31857149c910976198f2a624782c61a1
ulog4l <ulog4l@gmail.com>
2024-01-14 12:53:10 +0100

    cromulent keyboard

---
+++ keyboards/handwired/cromulent/keymaps/default/keymap.c
@@ -0,0 +1,66 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │
+     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [1] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [2] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [3] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [4] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [5] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [6] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    ),
+    [7] = LAYOUT_ortho(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,
+        KC_A,             KC_C,             KC_E,    KC_F,             KC_H,             KC_J
+    )
+};
---
+++ keyboards/handwired/cromulent/keymaps/vial/config.h
@@ -0,0 +1,24 @@
+// Copyright 2024 ulog4l (@ulog4l)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//Vial
+#define VIAL_KEYBOARD_UID {0x72, 0x74, 0x72, 0xE9, 0x28, 0x99, 0x01, 0x40}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,1}
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
